230a1a38bc3f00fb3715e4f2385a981ac93479e4
Nikita parfenov <n1koss.boy@gmail.com>
2020-05-18 23:38:15 +0500

    MVP

--- crypto-app-university/crypto-app-university/main_screen.Designer.cs
+++ crypto-app-university/crypto-app-university/main_screen.Designer.cs
@@ -28,17 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main_screen));
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.openFileBtn = new System.Windows.Forms.Button();
             this.pqInput = new System.Windows.Forms.TextBox();
             this.pqLabel = new System.Windows.Forms.Label();
-            this.publicKeyLabel = new System.Windows.Forms.Label();
             this.privteKeyLabel = new System.Windows.Forms.Label();
-            this.publicKeyTextBox = new System.Windows.Forms.TextBox();
             this.privateKeyTextBox = new System.Windows.Forms.TextBox();
             this.encryptMsgTextBox = new System.Windows.Forms.TextBox();
-            this.outputBtn = new System.Windows.Forms.Button();
+            this.saveFileBtn_encrypt = new System.Windows.Forms.Button();
             this.progressLabel = new System.Windows.Forms.Label();
+            this.dnInput = new System.Windows.Forms.TextBox();
+            this.dnLabel = new System.Windows.Forms.Label();
+            this.openFileBtn2 = new System.Windows.Forms.Button();
+            this.decryptMsgTextBox = new System.Windows.Forms.TextBox();
+            this.saveFileBtn_decrypt = new System.Windows.Forms.Button();
+            this.progressLabel2 = new System.Windows.Forms.Label();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // openFileDialog
@@ -49,7 +57,7 @@
             // 
             this.openFileBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.openFileBtn.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.openFileBtn.Location = new System.Drawing.Point(13, 73);
+            this.openFileBtn.Location = new System.Drawing.Point(15, 102);
             this.openFileBtn.Name = "openFileBtn";
             this.openFileBtn.Size = new System.Drawing.Size(283, 32);
             this.openFileBtn.TabIndex = 0;
@@ -61,55 +69,37 @@
             // 
             this.pqInput.BackColor = System.Drawing.Color.White;
             this.pqInput.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.pqInput.Location = new System.Drawing.Point(13, 38);
+            this.pqInput.Location = new System.Drawing.Point(15, 67);
             this.pqInput.Name = "pqInput";
             this.pqInput.Size = new System.Drawing.Size(283, 29);
             this.pqInput.TabIndex = 1;
+            this.pqInput.Text = "113 7";
             // 
             // pqLabel
             // 
             this.pqLabel.AutoSize = true;
             this.pqLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.pqLabel.Location = new System.Drawing.Point(9, 9);
+            this.pqLabel.Location = new System.Drawing.Point(11, 38);
             this.pqLabel.Name = "pqLabel";
             this.pqLabel.Size = new System.Drawing.Size(296, 21);
             this.pqLabel.TabIndex = 2;
             this.pqLabel.Text = "Введите простые p и q через пробел:";
             // 
-            // publicKeyLabel
-            // 
-            this.publicKeyLabel.AutoSize = true;
-            this.publicKeyLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.publicKeyLabel.Location = new System.Drawing.Point(9, 115);
-            this.publicKeyLabel.Name = "publicKeyLabel";
-            this.publicKeyLabel.Size = new System.Drawing.Size(92, 21);
-            this.publicKeyLabel.TabIndex = 3;
-            this.publicKeyLabel.Text = "public key: ";
-            // 
             // privteKeyLabel
             // 
             this.privteKeyLabel.AutoSize = true;
             this.privteKeyLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.privteKeyLabel.Location = new System.Drawing.Point(9, 150);
+            this.privteKeyLabel.Location = new System.Drawing.Point(14, 143);
             this.privteKeyLabel.Name = "privteKeyLabel";
-            this.privteKeyLabel.Size = new System.Drawing.Size(94, 21);
+            this.privteKeyLabel.Size = new System.Drawing.Size(37, 21);
             this.privteKeyLabel.TabIndex = 4;
-            this.privteKeyLabel.Text = "private key:";
-            // 
-            // publicKeyTextBox
-            // 
-            this.publicKeyTextBox.BackColor = System.Drawing.Color.White;
-            this.publicKeyTextBox.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.publicKeyTextBox.Location = new System.Drawing.Point(142, 112);
-            this.publicKeyTextBox.Name = "publicKeyTextBox";
-            this.publicKeyTextBox.Size = new System.Drawing.Size(154, 29);
-            this.publicKeyTextBox.TabIndex = 5;
+            this.privteKeyLabel.Text = "d n:";
             // 
             // privateKeyTextBox
             // 
             this.privateKeyTextBox.BackColor = System.Drawing.Color.White;
             this.privateKeyTextBox.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.privateKeyTextBox.Location = new System.Drawing.Point(142, 147);
+            this.privateKeyTextBox.Location = new System.Drawing.Point(143, 140);
             this.privateKeyTextBox.Name = "privateKeyTextBox";
             this.privateKeyTextBox.Size = new System.Drawing.Size(154, 29);
             this.privateKeyTextBox.TabIndex = 6;
@@ -118,52 +108,144 @@
             // 
             this.encryptMsgTextBox.BackColor = System.Drawing.Color.White;
             this.encryptMsgTextBox.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.encryptMsgTextBox.Location = new System.Drawing.Point(13, 182);
+            this.encryptMsgTextBox.Location = new System.Drawing.Point(15, 175);
             this.encryptMsgTextBox.Multiline = true;
             this.encryptMsgTextBox.Name = "encryptMsgTextBox";
             this.encryptMsgTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.encryptMsgTextBox.Size = new System.Drawing.Size(282, 89);
             this.encryptMsgTextBox.TabIndex = 7;
             // 
-            // outputBtn
+            // saveFileBtn_encrypt
             // 
-            this.outputBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.outputBtn.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.outputBtn.Location = new System.Drawing.Point(13, 297);
-            this.outputBtn.Name = "outputBtn";
-            this.outputBtn.Size = new System.Drawing.Size(283, 32);
-            this.outputBtn.TabIndex = 8;
-            this.outputBtn.Text = "Сохранить в файл";
-            this.outputBtn.UseVisualStyleBackColor = true;
+            this.saveFileBtn_encrypt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.saveFileBtn_encrypt.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.saveFileBtn_encrypt.Location = new System.Drawing.Point(14, 297);
+            this.saveFileBtn_encrypt.Name = "saveFileBtn_encrypt";
+            this.saveFileBtn_encrypt.Size = new System.Drawing.Size(283, 32);
+            this.saveFileBtn_encrypt.TabIndex = 8;
+            this.saveFileBtn_encrypt.Text = "Сохранить в файл";
+            this.saveFileBtn_encrypt.UseVisualStyleBackColor = true;
+            this.saveFileBtn_encrypt.Click += new System.EventHandler(this.saveFileBtn_encrypt_Click);
             // 
             // progressLabel
             // 
             this.progressLabel.AutoSize = true;
-            this.progressLabel.Location = new System.Drawing.Point(12, 274);
+            this.progressLabel.Location = new System.Drawing.Point(11, 245);
             this.progressLabel.Name = "progressLabel";
             this.progressLabel.Size = new System.Drawing.Size(0, 13);
             this.progressLabel.TabIndex = 9;
+            // 
+            // dnInput
+            // 
+            this.dnInput.BackColor = System.Drawing.Color.White;
+            this.dnInput.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dnInput.Location = new System.Drawing.Point(326, 67);
+            this.dnInput.Name = "dnInput";
+            this.dnInput.Size = new System.Drawing.Size(283, 29);
+            this.dnInput.TabIndex = 10;
+            // 
+            // dnLabel
+            // 
+            this.dnLabel.AutoSize = true;
+            this.dnLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dnLabel.Location = new System.Drawing.Point(322, 38);
+            this.dnLabel.Name = "dnLabel";
+            this.dnLabel.Size = new System.Drawing.Size(37, 21);
+            this.dnLabel.TabIndex = 11;
+            this.dnLabel.Text = "d n:";
+            // 
+            // openFileBtn2
+            // 
+            this.openFileBtn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.openFileBtn2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.openFileBtn2.Location = new System.Drawing.Point(326, 102);
+            this.openFileBtn2.Name = "openFileBtn2";
+            this.openFileBtn2.Size = new System.Drawing.Size(283, 32);
+            this.openFileBtn2.TabIndex = 12;
+            this.openFileBtn2.Text = "Выберите файл .txt";
+            this.openFileBtn2.UseVisualStyleBackColor = true;
+            this.openFileBtn2.Click += new System.EventHandler(this.openFileBtn2_Click);
+            // 
+            // decryptMsgTextBox
+            // 
+            this.decryptMsgTextBox.BackColor = System.Drawing.Color.White;
+            this.decryptMsgTextBox.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.decryptMsgTextBox.Location = new System.Drawing.Point(326, 140);
+            this.decryptMsgTextBox.Multiline = true;
+            this.decryptMsgTextBox.Name = "decryptMsgTextBox";
+            this.decryptMsgTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.decryptMsgTextBox.Size = new System.Drawing.Size(282, 124);
+            this.decryptMsgTextBox.TabIndex = 13;
+            // 
+            // saveFileBtn_decrypt
+            // 
+            this.saveFileBtn_decrypt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.saveFileBtn_decrypt.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.saveFileBtn_decrypt.Location = new System.Drawing.Point(326, 297);
+            this.saveFileBtn_decrypt.Name = "saveFileBtn_decrypt";
+            this.saveFileBtn_decrypt.Size = new System.Drawing.Size(283, 32);
+            this.saveFileBtn_decrypt.TabIndex = 14;
+            this.saveFileBtn_decrypt.Text = "Сохранить в файл";
+            this.saveFileBtn_decrypt.UseVisualStyleBackColor = true;
+            this.saveFileBtn_decrypt.Click += new System.EventHandler(this.saveFileBtn_decrypt_Click);
+            // 
+            // progressLabel2
+            // 
+            this.progressLabel2.AutoSize = true;
+            this.progressLabel2.Location = new System.Drawing.Point(323, 274);
+            this.progressLabel2.Name = "progressLabel2";
+            this.progressLabel2.Size = new System.Drawing.Size(0, 13);
+            this.progressLabel2.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(119, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 21);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "ENCRYPT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(438, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 21);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "DECRYPT";
             // 
             // main_screen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(307, 341);
+            this.ClientSize = new System.Drawing.Size(621, 336);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressLabel2);
+            this.Controls.Add(this.saveFileBtn_decrypt);
+            this.Controls.Add(this.decryptMsgTextBox);
+            this.Controls.Add(this.openFileBtn2);
+            this.Controls.Add(this.dnLabel);
+            this.Controls.Add(this.dnInput);
             this.Controls.Add(this.progressLabel);
-            this.Controls.Add(this.outputBtn);
+            this.Controls.Add(this.saveFileBtn_encrypt);
             this.Controls.Add(this.encryptMsgTextBox);
             this.Controls.Add(this.privateKeyTextBox);
-            this.Controls.Add(this.publicKeyTextBox);
             this.Controls.Add(this.privteKeyLabel);
-            this.Controls.Add(this.publicKeyLabel);
             this.Controls.Add(this.pqLabel);
             this.Controls.Add(this.pqInput);
             this.Controls.Add(this.openFileBtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(637, 371);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(637, 371);
             this.Name = "main_screen";
-            this.ShowIcon = false;
-            this.ShowInTaskbar = false;
-            this.Text = "RSA";
+            this.Text = "RSA Парфенов ИТк-301";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -175,13 +257,20 @@
         private System.Windows.Forms.Button openFileBtn;
         private System.Windows.Forms.TextBox pqInput;
         private System.Windows.Forms.Label pqLabel;
-        private System.Windows.Forms.Label publicKeyLabel;
         private System.Windows.Forms.Label privteKeyLabel;
-        private System.Windows.Forms.TextBox publicKeyTextBox;
         private System.Windows.Forms.TextBox privateKeyTextBox;
         private System.Windows.Forms.TextBox encryptMsgTextBox;
-        private System.Windows.Forms.Button outputBtn;
+        private System.Windows.Forms.Button saveFileBtn_encrypt;
         private System.Windows.Forms.Label progressLabel;
+        private System.Windows.Forms.TextBox dnInput;
+        private System.Windows.Forms.Label dnLabel;
+        private System.Windows.Forms.Button openFileBtn2;
+        private System.Windows.Forms.TextBox decryptMsgTextBox;
+        private System.Windows.Forms.Button saveFileBtn_decrypt;
+        private System.Windows.Forms.Label progressLabel2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- crypto-app-university/crypto-app-university/main_screen.cs
+++ crypto-app-university/crypto-app-university/main_screen.cs
@@ -24,9 +24,8 @@ namespace crypto_app_university
 
         private List<string> encryptRSA(string msg, long e, long n)
         {
-            MessageBox.Show("Start Encrypt!");
             int msgLen = msg.Length;
-            progressLabel.Text = $"Progress ({0}/{msgLen})";
+            progressLabel.Text = $"Progress (0/{msgLen})";
             List<string> result = new List<string>();
             BigInteger bi;
             for (int i = 0; i < msgLen; i++) {
@@ -44,18 +43,23 @@ namespace crypto_app_university
         private string decryptRSA(List<string> input, long d, long n)
         {
             string result = "";
+            int index = 0;
+            int msgLen = input.Count;
+            progressLabel2.Text = $"Progress ({index}/{msgLen})";
+
             BigInteger bi;
             foreach (string item in input) {
+                index += 1;
                 bi = new BigInteger(Convert.ToDouble(item));
                 bi = BigInteger.Pow(bi, (int)d);
                 BigInteger n_ = new BigInteger((int)n);
                 bi %= n_;
                 int i = Convert.ToInt32(bi.ToString());
                 result += alphabet[i].ToString();
+                progressLabel2.Text = $"Progress ({index}/{msgLen})";
             }
             return result;
         }
-
 
         private long getD(long m)
         {
@@ -70,7 +74,7 @@ namespace crypto_app_university
 
         private long getE(long d, long m)
         {
-            long e = 10;    
+            long e = 0;    
             while (true) {
                 if ((e * d) % m == 1) break;
                 else e++;
@@ -87,7 +91,14 @@ namespace crypto_app_university
             return true;
         }
 
-        private bool isValidInput(string input)
+        private bool isValidDNInput(string input)
+        {
+            if (input.Length > 0 && input.Split(' ').Length == 2)
+                return true;
+            return false;
+        }
+
+        private bool isValidPQInput(string input)
         {
             if (input.Length > 0 && input.Split(' ').Length == 2)
             {
@@ -96,9 +107,7 @@ namespace crypto_app_university
                 first = Convert.ToInt64(pq[0]);
                 second = Convert.ToInt64(pq[1]);
                 if (isPrime(first) && isPrime(second))
-                {
                     return true;
-                }
             }
             return false;
         }
@@ -108,19 +117,19 @@ namespace crypto_app_university
             /* [A-Z][a-z][А-Я][а-я][0-9] */
             char[] alphabet_en = Enumerable.Range('A', 58).Select(x => (char)x).ToArray();
             char[] alphabet_ru = Enumerable.Range('А', 64).Select(x => (char)x).ToArray();
-            char[] alphabet_sumbols = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Е', 'Ё', '#', ' ', '\r', '\n' };
+            char[] alphabet_sumbols = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Е', 'Ё', '#', ' ', '\r', '\n', ',', '.', '?', '!' };
             char[] alphabet = new char[alphabet_ru.Length + alphabet_en.Length + alphabet_sumbols.Length];
             alphabet_en.CopyTo(alphabet, 0);
             alphabet_ru.CopyTo(alphabet, alphabet_en.Length);
             alphabet_sumbols.CopyTo(alphabet, alphabet_en.Length + alphabet_ru.Length);
+            //MessageBox.Show(new string(alphabet), "accepted alphabet");
             return alphabet;
         }
 
         private void openFileBtn_Click(object sender, EventArgs e)
         {
-            if (isValidInput(pqInput.Text))
+            if (isValidPQInput(pqInput.Text))
             {
-                MessageBox.Show("Is Valid!");
                 long p_, q_;
                 string[] pq = pqInput.Text.Split(' ');
                 p_ = Convert.ToInt64(pq[0]);
@@ -131,7 +140,66 @@ namespace crypto_app_university
 
                 using (OpenFileDialog openFileDialog = new OpenFileDialog())
                 {
-                    openFileDialog.InitialDirectory = "c:\\";
+                    //openFileDialog.InitialDirectory = "c:\\";
+                    openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                    openFileDialog.FilterIndex = 2;
+                    openFileDialog.RestoreDirectory = true;
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        inputFilePath = openFileDialog.FileName;
+                        Stream inputFileStream = openFileDialog.OpenFile();
+                        using (StreamReader reader = new StreamReader(inputFileStream, Encoding.Default))
+                            inputText = reader.ReadToEnd();
+                    }
+                }
+
+                //byte[] toEncodeAsBytes = Encoding.UTF8.GetBytes(inputText);
+                //inputText = Convert.ToBase64String(toEncodeAsBytes);
+
+
+                long n_ = p_ * q_;
+                long m_ = (p_ - 1) * (q_ - 1);
+                long d_ = getD(m_);
+                long e_ = getE(d_, m_);
+
+                privateKeyTextBox.Text = $"{d_} {n_}";
+                dnInput.Text = $"{d_} {n_}";
+
+                List<string> encryptMsg = encryptRSA(inputText, e_, n_);
+                encryptMsgTextBox.Text = "";
+                foreach (var line in encryptMsg)
+                    encryptMsgTextBox.Text += $"{line}\r\n";
+            }
+            else
+                pqLabel.ForeColor = Color.Red;
+        }
+
+        private void saveFileBtn_encrypt_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                FileName = "encrypt",
+                DefaultExt = ".txt",
+                Filter = "Text documents (.txt)|*.txt"
+            };
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                File.WriteAllText(saveFileDialog.FileName, encryptMsgTextBox.Text, Encoding.GetEncoding("iso-8859-1"));
+        }
+
+        private void openFileBtn2_Click(object sender, EventArgs e)
+        {
+            if (isValidDNInput(dnInput.Text))
+            {
+                long d_, n_;
+                d_ = Convert.ToInt64(dnInput.Text.Split(' ')[0]);
+                n_ = Convert.ToInt64(dnInput.Text.Split(' ')[1]);
+
+                List<string> inputText = new List<string>();
+                string inputFilePath = "";
+
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    //openFileDialog.InitialDirectory = "c:\\";
                     openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
                     openFileDialog.FilterIndex = 2;
                     openFileDialog.RestoreDirectory = true;
@@ -140,35 +208,26 @@ namespace crypto_app_university
                         inputFilePath = openFileDialog.FileName;
                         Stream inputFileStream = openFileDialog.OpenFile();
                         using (StreamReader reader = new StreamReader(inputFileStream))
-                        {
-                            inputText = reader.ReadToEnd();
-                        }
+                            while (!reader.EndOfStream)
+                                inputText.Add(reader.ReadLine());
                     }
                 }
-                long n_ = p_ * q_;
-                long m_ = (p_ - 1) * (q_ - 1);
-                MessageBox.Show("N M!");
-
-                long d_ = getD(m_);
-                MessageBox.Show("D!");
-
-                long e_ = getE(d_, m_);
-                MessageBox.Show("E!");
-
-
-                publicKeyTextBox.Text = $"{e_} {n_}";
-                privateKeyTextBox.Text = $"{d_} {n_}";
-
-                List<string> encryptMsg = encryptRSA(inputText, e_, n_);
-                foreach (var line in encryptMsg)
-                {
-                    encryptMsgTextBox.Text += $"{line}\r\n";
-                }
+                string decryptMsg = decryptRSA(inputText, d_, n_);
+                decryptMsgTextBox.Text = "";
+                decryptMsgTextBox.Text = decryptMsg;
             }
-            else
+        }
+
+        private void saveFileBtn_decrypt_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
             {
-                pqLabel.ForeColor = Color.Red;
-            }
+                FileName = "decrypt",
+                DefaultExt = ".txt",
+                Filter = "Text documents (.txt)|*.txt"
+            };
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                File.WriteAllText(saveFileDialog.FileName, decryptMsgTextBox.Text, Encoding.GetEncoding("iso-8859-1"));
         }
     }
 }
